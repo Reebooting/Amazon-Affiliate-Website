5d9afc8cd0a249e0fdf6621b543770d07a1c96e2
Reebooting <87072968+Reebooting@users.noreply.github.com>
2024-02-02 00:42:52 +0530

    second push

--- amazon-clone-frontend-main/src/axios.js
+++ amazon-clone-frontend-main/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://amazon-affiliate-website.onrender.com/",
 });
 
 export default instance;
